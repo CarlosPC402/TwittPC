4bb49bea6d528a39836b6c68dd580e7ce9da26bc
Carlos De Jesus Puc Cauich <cpuc@aspel.com.mx>
2022-12-15 16:57:53 -0600

    Eliminar tweet

---
+++ bd/borroTweet.go
@@ -0,0 +1,28 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/* BorroTweet borra el tweet de la bd, el primer parámetro es el id del tweet a borrar */
+func BorroTweet(ID string, UserID string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	db := MongoCN.Database("twittpc")
+	col := db.Collection("tweet")
+
+	objID, _ := primitive.ObjectIDFromHex(ID)
+
+	condicion := bson.M{
+		"_id":    objID,
+		"userid": UserID,
+	}
+
+	_, err := col.DeleteOne(ctx, condicion)
+
+	return err
+}
